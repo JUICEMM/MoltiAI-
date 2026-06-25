e28e8541d2a22e70e01c13983a1ea1cf52180aaf
JUICEMM <56994401+JUICEMM@users.noreply.github.com>
2026-06-25 19:24:46 +0800

    Update remotion.config.ts

--- MoltiAI-/vidgo-video-prototype/remotion.config.ts
+++ MoltiAI-/vidgo-video-prototype/remotion.config.ts
@@ -1,4 +1,7 @@
 import {Config} from '@remotion/cli/config';
 
+Config.setConcurrency(1);
+Config.setDisallowParallelEncoding(true);
+Config.setRendererPort(49152);
 Config.setVideoImageFormat('jpeg');
 Config.setOverwriteOutput(true);
